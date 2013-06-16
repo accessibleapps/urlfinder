57ff6cfdac3d96209de900bca5c1db09069406d7
Q <q@q-continuum.net>
2013-06-16 02:39:23 -0600

    Find URLs from a string

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from urlfinder import __doc__, __version__
+
+setup(
+ name = 'urlfinder',
+ version = __version__,
+ description = __doc__,
+ py_modules = ['urlfinder',],
+ install_requires = [
+  'setuptools',
+ ],
+ classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'Topic :: Software Development :: Libraries',
+ ],
+)
